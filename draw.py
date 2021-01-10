21f43cd8411db2f588cddb6a3547088e8512a804
shwetamasrani <shwetamasrani19@gmail.com>
2021-01-10 10:22:31 +0530

    Drawing a rectangle

--- draw.py
+++ draw.py
@@ -7,6 +7,11 @@ cv.imshow('Blank', blank)
 # cv.imshow('Cat', img)
 
 # 1. Paint the image a certain colour
-blank[:] = 0,0,255
-cv.imshow('Green',blank)
+# blank[200:300,100:400] = 0,0,255
+# cv.imshow('Green',blank)
+
+# 2. Draw a rectangle
+cv.rectangle(blank, (0,0), (250,500), (0,255,0), thickness = 2)
+cv.imshow('Rectangle',blank)
+
 cv.waitKey(0)
