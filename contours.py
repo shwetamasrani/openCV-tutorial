6712a736644564bee5b69d2517408289b1759dcb
shwetamasrani <shwetamasrani19@gmail.com>
2021-01-10 16:28:57 +0530

    Finding contours using canny edges

---
+++ contours.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Resources/Photos/cats.jpg')
+cv.imshow('Cats', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+print(f'{len(contours)} contours found:')
+
+cv.waitKey(0)
