be368c5163a9f5f64c41692d77beb7d34c72a423
shwetamasrani <shwetamasrani19@gmail.com>
2021-01-10 11:12:52 +0530

    Resizing

--- basic.py
+++ basic.py
@@ -12,7 +12,19 @@ blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 cv.imshow('Blur', blur)
 
 # Edge Cascade
-canny = cv.Canny(img, 125, 175)
+canny = cv.Canny(blur, 125, 175)
 cv.imshow('Canny Edges', canny)
+
+# Dilating the image
+dilated = cv.dilate(canny, (7,7), iterations=3)
+cv.imshow('Dilated', dilated)
+
+# Eroding
+eroded = cv.erode(dilated, (3,3), iterations = 3)
+cv.imshow('Eroded', eroded)
+
+# Resize
+resized = cv.resize(img, (500,500), interpolation = cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
 
 cv.waitKey(0)
