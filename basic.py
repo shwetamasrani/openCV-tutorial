ec77ca294175cee24e86a4e4f7b49099a6ea87f2
shwetamasrani <shwetamasrani19@gmail.com>
2021-01-10 10:57:42 +0530

    Blurring an image

--- basic.py
+++ basic.py
@@ -7,5 +7,8 @@ cv.imshow('Cat',img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+# Blur
+blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
 
 cv.waitKey(0)
