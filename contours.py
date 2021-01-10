27d71ebc4fd4dbb194f0b2d4b2138c1e397fa66d
shwetamasrani <shwetamasrani19@gmail.com>
2021-01-10 16:31:53 +0530

    Thresholding

--- contours.py
+++ contours.py
@@ -7,13 +7,16 @@ cv.imshow('Cats', img)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
-blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
-cv.imshow('Blur', blur)
+# blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
+# cv.imshow('Blur', blur)
 
-canny = cv.Canny(blur, 125, 175)
-cv.imshow('Canny Edges', canny)
+# canny = cv.Canny(blur, 125, 175)
+# cv.imshow('Canny Edges', canny)
 
-contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+cv.imshow('Thresh', thresh)
+
+contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
 print(f'{len(contours)} contours found:')
 
