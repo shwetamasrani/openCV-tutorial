d0efaa666527a6b295d6090fb96a8bbc41be1da0
shwetamasrani <shwetamasrani19@gmail.com>
2021-01-10 10:05:58 +0530

    Rescaling images and videos

---
+++ rescale.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+
+def rescaleFrame(frame,scale=0.75):
+    # Images, Videos and live videos
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation= cv.INTER_AREA)
+
+def changeRes(width,height):
+    # Live video
+    capture.set(3,width)
+    capture.set(4,height)
+
+# Reading Image
+img = cv.imread('Resources/Photos/cat.jpg')
+cv.imshow('Cat',img)
+resized_img = rescaleFrame(img)
+cv.imshow('Image',resized_img)
+
+# Reading Videos
+capture = cv.VideoCapture('Resources/Videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+
+    frame_resized = rescaleFrame(frame,scale=0.2)
+    cv.imshow('Video',frame)
+    cv.imshow('Video_resized',frame_resized)
+    
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
